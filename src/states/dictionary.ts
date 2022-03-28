ad3179a08d6f4011e1337791d5ad37b3ffb37f34
shu.nonaka <shu.nonaka@leverages.jp>
2022-03-28 17:34:47 +0900

    wip

--- src/states/dictionary.ts
+++ src/states/dictionary.ts
@@ -24,14 +24,103 @@ export const Dictionary = selector<Word[]>({
   key: "Dictionary",
   get: async ({get}) => {
     get(RenderDispatcher)
-    const db = await openDB("flashcard", 1,{
-      upgrade(db) {
+    const db = await openDB("flashcard", 1, {
+      async upgrade(db) {
         db.createObjectStore("words")
       }
     })
 
     const pagination = get(Pagination)
 
+    const count = await db.count("words")
+
+    if (count < 1) {
+      await Promise.all(firstWords.map(w => db.add("words", w, w.en)))
+    }
+
     return db.getAll("words", null, pagination.currentPage * 20)
   },
 })
+
+const firstWords = [
+  {
+    en: "abroad",
+    ja: "外国で"
+  },
+  {
+    en: "bear",
+    ja: "クマ"
+  },
+  {
+    en: "become",
+    ja: "〜になる"
+  },
+  {
+    en: "center",
+    ja: "中央"
+  },
+  {
+    en: "chalk",
+    ja: "チョーク"
+  },
+  {
+    en: "dry",
+    ja: "乾く"
+  },
+  {
+    en: "dump",
+    ja: "ゴミ捨て場"
+  },
+  {
+    en: "ear",
+    ja: "耳"
+  },
+  {
+    en: "else",
+    ja: "他に"
+  },
+  {
+    en: "far",
+    ja: "遠く"
+  },
+  {
+    en: "grass",
+    ja: "草"
+  },
+  {
+    en: "grade",
+    ja: "学年"
+  },
+  {
+    en: "heart",
+    ja: "心"
+  },
+  {
+    en: "invite",
+    ja: "招待する"
+  },
+  {
+    en: "key",
+    ja: "鍵"
+  },
+  {
+    en: "moon",
+    ja: "月"
+  },
+  {
+    en: "pencil",
+    ja: "鉛筆"
+  },
+  {
+    en: "right",
+    ja: "正しい"
+  },
+  {
+    en: "shop",
+    ja: "店"
+  },
+  {
+    en: "test",
+    ja: "試験"
+  },
+]
